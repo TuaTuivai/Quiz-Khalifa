c7c3c71908a081d93e2284c1df67b5eb2ef5f751
TuaTuivai <TuaMTuivai@gmail.com>
2022-11-10 12:41:50 -0800

    finished quiz goes into score screen

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,28 +1,43 @@
-var finishLine     = document.querySelector('.scoreboard');
-var scoreScreen    = document.querySelector('.score-screen')
-var scoreBoard     = document.querySelector('.highscore-button');
-var questions      = document.querySelector('.questions');
+var finishLine     = document.querySelector('.quiz-finished');
+var scoreScreen    = document.querySelector('.score-screen');
+
+
 var header         = document.querySelector('.welcome-screen');
 var scoreAndTimer  = document.querySelector('.high-score-timer-bar');
 var outerContainer = document.querySelector('.outer-container');
 var innerContainer = document.querySelector('.inner-container');
-var submit         = document.querySelector('.submit');
-var choices        = document.querySelector('.choices'); 
-var startButton    = document.querySelector('.start-quiz-button');
-var question       = document.querySelector('#question');
-var choice1        = document.querySelector('#choice1') 
-var choice2        = document.querySelector('#choice2') 
-var choice3        = document.querySelector('#choice3') 
-var choice4        = document.querySelector('#choice4') 
-var currentIndex   = 0
-var timeLeft       = 60
-var timer          
-var countDown      = document.querySelector('#countdown')
-var backButton     = document.querySelector('.backbutton')
 
-function myFunction (){
+// button
+
+var submit         = document.querySelector('.submit');
+var scoreBoard     = document.querySelector('.highscore-button');
+var backButton     = document.querySelector('.backbutton');
+var startButton    = document.querySelector('.start-quiz-button');
+
+// Choices and Question
+
+var questions      = document.querySelector('.question-content');
+var question       = document.querySelector('#question');
+var choices        = document.querySelector('.choices'); 
+var choice1        = document.querySelector('#choice1');
+var choice2        = document.querySelector('#choice2');
+var choice3        = document.querySelector('#choice3');
+var choice4        = document.querySelector('#choice4');
+var currentIndex   = 0;
+var score          = 0;
+
+// timer var
+
+var timeLeft       = 60;
+var timer          
+var countDown      = document.querySelector('#countdown');
+
+// Functions
+
+function startQuiz (){
     header.style.display="none"
     questions.style.display='block'
+    scoreBoard.style.display='none'
     startTimer()
     changeQuestion()
 };
@@ -30,36 +45,72 @@ function myFunction (){
 function highScoreButton(){
     header.style.display='none'
     question.style.display='none'
-    scoreScreen.style.display='block'};
+    choices.style.display='none'
+    scoreScreen.style.display='block'
+};
 
-function endOfQuiz (){
-     questions.style.display='none'
-    finishLine.style.display='block'
-    };
-    
+
 function backToQuiz (){
-    myFunction()
+    startQuiz()
     scoreScreen.style.display='none'
     question.style.display='block'
+    choices.style.display='flex-direction'
 
-// needs to finish final question onto final screen with score and input text
-};
-function Done(){
-    quizContent[0],choices[3]
-    endOfQuiz()
 };
 
-// function overallScoreStuff (){
+function endOfQuiz (){
+    questions.style.display='none'
+    finishLine.style.display='block'
+    clearInterval(timer)
+ 
+};
 
+// function wrongMinus5 (){
+//     timeLeft -- 
 // }
 
 
 
-startButton.addEventListener("click", myFunction);
-scoreBoard.addEventListener('click', highScoreButton);
-backButton.addEventListener('click',backToQuiz);
-// choice3.addEventListener('click',endOfQuiz);
-// supposed to be any answer instead of choice
+function clickAnswer(event){
+    var userAnswer    = (event.target.textContent)
+    var correctAnswer = quizContent[currentIndex].answer
+    if (userAnswer == correctAnswer){
+        alert('correct')
+        
+    }
+    else {
+        alert('wrong')
+    }
+    currentIndex++
+    if (currentIndex == 4){
+        endOfQuiz()
+    }
+    else{
+    changeQuestion()
+    }
+    // if ( countDown == 0){
+    //     setTimeout((,) => {
+            
+    //     }, timeout);
+    // }
+};
+
+    function startTimer (){
+    timer = setInterval(function(){
+    timeLeft--
+    countDown.textContent='Timer: '+ timeLeft}, 1000)
+    // if (userAnswer == wrong){
+    //     timeLeft -- 
+    // }
+};
+    function changeQuestion (){
+    question.textContent=quizContent[currentIndex].question
+    choice1.textContent=quizContent[currentIndex].choices[0]
+    choice2.textContent=quizContent[currentIndex].choices[1]
+    choice3.textContent=quizContent[currentIndex].choices[2]
+    choice4.textContent=quizContent[currentIndex].choices[3]
+};
+
 var quizContent= [
     {
         question: "1. Which tag should typically be at the bottom of your HTML page?",
@@ -84,37 +135,11 @@ var quizContent= [
  
 ];
 
-function changeQuestion (){
-    question.textContent=quizContent[currentIndex].question
-    choice1.textContent=quizContent[currentIndex].choices[0]
-    choice2.textContent=quizContent[currentIndex].choices[1]
-    choice3.textContent=quizContent[currentIndex].choices[2]
-    choice4.textContent=quizContent[currentIndex].choices[3]
-};
-function clickAnswer(event){
-    var userAnswer    = (event.target.textContent)
-    var correctAnswer = quizContent[currentIndex].answer
-    if (userAnswer == correctAnswer){
-        alert('correct')
-        
-    }
-    else {
-        alert('wrong')
-        timeLeft --
-    }
-    currentIndex++
-    changeQuestion()
-}
-
-function startTimer (){
-    timer = setInterval(function(){
-        timeLeft--
-        countDown.textContent='Timer: '+ timeLeft
-    }, 1000)
-}
-
-choice1.addEventListener('click',clickAnswer)
-choice2.addEventListener('click',clickAnswer)
-choice3.addEventListener('click',clickAnswer)
-choice4.addEventListener('click',clickAnswer)
+startButton.addEventListener("click", startQuiz);
+scoreBoard.addEventListener('click', highScoreButton);
+backButton.addEventListener('click',backToQuiz);
+choice1.addEventListener('click',clickAnswer);
+choice2.addEventListener('click',clickAnswer);
+choice3.addEventListener('click',clickAnswer);
+choice4.addEventListener('click',clickAnswer);
 
